ce081864206edd2dd6de4469f4668348b0b4572b
phyowaihtoon <phyogyi.java@gmail.com>
2022-08-16 22:32:51 +0800

    adding document inquiry service

---
+++ src/main/java/com/hmm/dms/service/impl/DocumentInquiryServiceImpl.java
@@ -0,0 +1,13 @@
+package com.hmm.dms.service.impl;
+
+import com.hmm.dms.service.DocumentInquiryService;
+import com.hmm.dms.service.dto.DocumentHeaderDTO;
+import java.util.List;
+
+public class DocumentInquiryServiceImpl implements DocumentInquiryService {
+
+    @Override
+    public List<DocumentHeaderDTO> searchDocumentsByRepoURL() {
+        return null;
+    }
+}
