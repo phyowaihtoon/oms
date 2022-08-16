9448f7d3721ecc0ecaf7d28476edf51d0260830f
phyowaihtoon <phyogyi.java@gmail.com>
2022-08-16 23:31:42 +0800

    adding document inquiry service

--- src/main/java/com/hmm/dms/service/impl/DocumentInquiryServiceImpl.java
+++ src/main/java/com/hmm/dms/service/impl/DocumentInquiryServiceImpl.java
@@ -3,7 +3,11 @@ package com.hmm.dms.service.impl;
 import com.hmm.dms.service.DocumentInquiryService;
 import com.hmm.dms.service.dto.DocumentHeaderDTO;
 import java.util.List;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Service
+@Transactional
 public class DocumentInquiryServiceImpl implements DocumentInquiryService {
 
     @Override
