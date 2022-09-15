98e033d05818cd418c7261c1d8787594eea6a6d4
KoKoAungMAB <88429658+KoKoAungMAB@users.noreply.github.com>
2022-09-15 14:33:24 +0630

    repository dialog update by kka

--- src/main/webapp/app/entities/document/document.module.ts
+++ src/main/webapp/app/entities/document/document.module.ts
@@ -5,9 +5,10 @@ import { DocumentUpdateComponent } from './update/document-update.component';
 import { DocumentComponent } from './list/document.component';
 import { DocumentDetailComponent } from './detail/document-detail.component';
 import { InfoPopupComponent } from '../util/infopopup/info-popup.component';
+import { RepositoryDialogComponent } from '../util/repositorypopup/repository-dialog.component';
 
 @NgModule({
   imports: [SharedModule, DocumentRoutingModule],
-  declarations: [DocumentUpdateComponent, DocumentComponent, DocumentDetailComponent, InfoPopupComponent],
+  declarations: [DocumentUpdateComponent, DocumentComponent, DocumentDetailComponent, InfoPopupComponent, RepositoryDialogComponent],
 })
 export class DocumentModule {}
