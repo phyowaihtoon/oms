a210a830e22cd00715cf0c9179bc7e0e0e42c348
phyowaihtoon <phyogyi.java@gmail.com>
2022-10-04 22:36:06 +0800

    Update File Upload

--- src/main/webapp/app/entities/util/reply-message.model.ts
+++ src/main/webapp/app/entities/util/reply-message.model.ts
@@ -5,13 +5,14 @@ export interface IReplyMessage {
 }
 
 export class ResponseCode {
-  static readonly SUCCESS_CODE: string = '000';
+  static readonly SUCCESS: string = '000';
   static readonly SUCCESS_MSG: string = 'SUCCESS';
 
-  static readonly ERROR_CODE: string = 'E00';
+  static readonly ERROR_E00: string = 'E00';
+  static readonly ERROR_E01: string = 'E01';
   static readonly ERROR_MSG: string = 'ERROR';
 
-  static readonly WARNING_CODE: string = 'W00';
+  static readonly WARNING: string = 'W00';
   static readonly WARNING_MSG: string = 'WARNING';
 
   static readonly RESPONSE_FAILED_CODE: string = 'R00';
